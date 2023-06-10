f30a7bf2e97947f1d0b5352b9b0e71605a46a6db
Phan Kien <phankien110302@gmail.com>
2023-06-10 22:58:49 +0700

    add route api get chung loai

--- server/src/routes/apiRoute.js
+++ server/src/routes/apiRoute.js
@@ -1,6 +1,8 @@
 import Express  from "express";
 import User from "../controllers/userController"
 import VatTu from "../controllers/vatuController";
+import ChungLoai from "../controllers/chungLoaiVatTuController";
+import KhuVuc from "../controllers/khuVucController";
 const Router = Express.Router();
 
 const ApiRouter = (app)=>{
@@ -8,6 +10,8 @@ const ApiRouter = (app)=>{
     Router.post("/login",User.LoginUser)
     Router.get("/users",User.getAllUser)
     Router.get("/vatus",VatTu.getAllVatTu)
+    Router.get("/chungLoais",ChungLoai.getALlLoaiVatTu)
+    Router.get("/khuvucs",KhuVuc.getAllKhuVuc)
     return app.use("/api",Router);
 }
 module.exports = ApiRouter;
