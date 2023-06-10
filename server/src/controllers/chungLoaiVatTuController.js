49c1382da28c24ea1ddb63b2537aeaa614a06775
Phan Kien <phankien110302@gmail.com>
2023-06-10 22:56:41 +0700

    add api get all chung loai vat tu

--- server/src/controllers/chungLoaiVatTuController.js
+++ server/src/controllers/chungLoaiVatTuController.js
@@ -1,8 +1,15 @@
-import LoaiVatTu from "../models/LoaiVatTu"
-
-const getALlLoaiVatTu = async () =>{
-    const ListChungLoai = await ChungLoaiVatTu.findAll({include :"LoaiVatTu"});
-    return {
-        message: " "
+import ChungLoaiVatTu from "../models/ChungLoaiVatTu"
+import ApiResult from "../configs/resultApi";
+const getALlLoaiVatTu = async (req,res) =>{
+    try {
+        const ListChungLoai = await ChungLoaiVatTu.findAll({include :"LoaiVatTu"});
+        const data = new ApiResult("get all ChungLoai thành công",ListChungLoai);
+        return res.status(200).json(data);
+    } catch (error) {
+        const data = new ApiResult("get all ChungLoai thất bại",[]);
+        return res.status(500).json(data);
     }
+}
+export default {
+    getALlLoaiVatTu
 }
