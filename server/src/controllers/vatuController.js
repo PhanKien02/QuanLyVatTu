95cf79efccfd3bc5b37c99e52156be155ad9cd69
Phan Kien <phankien110302@gmail.com>
2023-06-10 22:57:55 +0700

    config api result and add api create vattu

--- server/src/controllers/vatuController.js
+++ server/src/controllers/vatuController.js
@@ -6,9 +6,29 @@ const getAllVatTu = async (req,res)=>{
         return  res.status(200).json(response)
     }).catch(err =>{
         console.log(err);
-        return res.status(500).json({message: "get all vatu faild"})
+        const response= new  ApiResult("get all vattu success",[])
+        return  res.status(200).json(response)
+    })
+}
+const addVatTu = async (req,res) =>{
+    const newVT = req.body
+    await VatTu.create({
+        mVT: newVT.maVatTu,
+        tenVatTu: newVT.tenVatTu,
+        donViTinh: newVT.donViTinh,
+        LoaiVatTuId:newVT.loaiVattu,
+        soLuong : newVT.soLuong,
+        donGia: newVT.dongia,
+        khuVucId : newVT.khuvuc
+    }).then((res)=>{
+        const data = new ApiResult("thêm vật tư thành công",res);
+        return res.status(200).json(data);
+    }).catch((error)=>{
+        console.log(error);
+        const data = new ApiResult("thêm vật tư thất bại",null);
+        return res.status(200).json(data);
     })
 }
 export default {
-    getAllVatTu
+    getAllVatTu,addVatTu
 }
