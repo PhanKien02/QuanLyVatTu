d814518308b81965a1e5d29f199983ee931fba4e
Phan Kien <phankien110302@gmail.com>
2023-06-10 22:54:30 +0700

    modify router client page VatTu

--- client/src/Router.js
+++ client/src/Router.js
@@ -4,7 +4,7 @@ const Home = lazy(() => import("./layouts/home/home"));
 const Auth = lazy(() => import("./layouts/auth/auth"));
 const Login = lazy(() => import("./pages/auth/login-form/login-form"));
 const Register = lazy(() => import("./pages/auth/register-form/register-form"));
-const VatuComponent = lazy(() => import('./pages/main/vatTuPage/index/vatTu.component') );
+const VatuComponent = lazy(() => import('./pages/main/vatTuPage/index/vatTuComponent') );
 
 const WebRoute = () => {
     return (
