dd2827d5e92a133b0f3c1cdafcb0fd0f896830cb
Phan Kien <phankien110302@gmail.com>
2023-06-14 00:33:13 +0700

    modify api vattu

--- server/src/controllers/vatuController.js
+++ server/src/controllers/vatuController.js
@@ -10,25 +10,60 @@ const getAllVatTu = async (req,res)=>{
         return  res.status(200).json(response)
     })
 }
-const addVatTu = async (req,res) =>{
+const createVatTu = async (req,res) =>{
     const newVT = req.body
     await VatTu.create({
-        mVT: newVT.maVatTu,
+        mVT: newVT.mVT,
         tenVatTu: newVT.tenVatTu,
         donViTinh: newVT.donViTinh,
-        LoaiVatTuId:newVT.loaiVattu,
+        LoaiVatTuId:newVT.LoaiVatTuId,
         soLuong : newVT.soLuong,
-        donGia: newVT.dongia,
-        khuVucId : newVT.khuvuc
-    }).then((res)=>{
-        const data = new ApiResult("thêm vật tư thành công",res);
+        donGia: newVT.donGia,
+        khuVucId : newVT.khuVucId
+    }).then((response)=>{
+        const data = new ApiResult("thêm vật tư thành công",response);
         return res.status(200).json(data);
     }).catch((error)=>{
         console.log(error);
-        const data = new ApiResult("thêm vật tư thất bại",null);
+        const data = new ApiResult("thêm vật tư thất bại",error);
         return res.status(200).json(data);
     })
 }
+const updateVattu = async (req,res) =>{
+    const VTUpdate =req.body;
+    console.log(VTUpdate);
+    try {
+        const Vattu= await VatTu.update({
+            mVT: VTUpdate.mVT,
+            tenVatTu: VTUpdate.tenVatTu,
+            donViTinh: VTUpdate.donViTinh,
+            LoaiVatTuId:VTUpdate.LoaiVatTuId,
+            soLuong : VTUpdate.soLuong,
+            donGia: VTUpdate.donGia,
+            khuVucId : VTUpdate.khuVucId
+        },{  where: {
+            mVT : VTUpdate.mVT
+        }
+        });
+        const data = new ApiResult("cập nhật vật tư thành công",Vattu);
+        return res.status(200).json(data);
+    } catch (error) {
+        // console.log(error);
+        const data = new ApiResult("thêm vật tư thất bại",null);
+        return res.status(200).json(data);
+    }
+}
+const deleteVattu = async (req,res) =>{
+    try {
+        const deleteVT =   await VatTu.destroy({ where:{mVT :req.params.mVT }});
+        const data = new ApiResult("Xóa vật tư thành công",deleteVT)
+        return res.status(200).json(data);
+    } catch (error) {
+        console.log(error);
+        const data = new ApiResult("Xóa vật tư thất bại",error)
+        return res.status(500).json(data);
+    }
+}
 export default {
-    getAllVatTu,addVatTu
+    getAllVatTu,createVatTu,updateVattu,deleteVattu
 }
