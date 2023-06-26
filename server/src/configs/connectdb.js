f60cce8cf1991b1f181cdd8900ebd4ad3b02f172
Phan Kien <phankien110302@gmail.com>
2023-06-26 22:44:15 +0700

    config

--- server/src/configs/connectdb.js
+++ server/src/configs/connectdb.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USER,process.env.DATABASE_PASSWORD, {
-    host: 'localhost',
+    host: process.env.DATABASE_HOST,
     dialect: 'mysql',
     logging: false
 });
