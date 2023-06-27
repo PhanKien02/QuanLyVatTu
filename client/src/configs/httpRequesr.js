e4bd78e130a9acd829e5d835b959e37082997d2e
PhanKien02 <87196043+PhanKien02@users.noreply.github.com>
2023-06-27 13:05:52 +0700

    Update httpRequesr.js

--- client/src/configs/httpRequesr.js
+++ client/src/configs/httpRequesr.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const baseURLDev = "http://localhost:8081/api/"
-const baseURLProduct = "https://serverquanlyvattu.onrender.com"
+const baseURLProduct = "https://serverquanlyvattu.onrender.com/api/"
 const instance = axios.create({
     baseURL: baseURLProduct  ,
     timeout: 1000,
