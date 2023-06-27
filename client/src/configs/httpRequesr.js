e10370028947dbbecfb42524129525a75b609293
Phan Kien <phankien110302@gmail.com>
2023-06-27 10:59:43 +0700

    config base url

--- client/src/configs/httpRequesr.js
+++ client/src/configs/httpRequesr.js
@@ -1,7 +1,9 @@
 import axios from "axios"
 
+const baseURLDev = "http://localhost:8081/api/"
+const baseURLProduct = "http://localhost:8081/api/"
 const instance = axios.create({
-    baseURL: 'http://localhost:8081/api/',
+    baseURL: baseURLDev  ,
     timeout: 1000,
     headers: {'X-Custom-Header': 'foobar'}
 });
