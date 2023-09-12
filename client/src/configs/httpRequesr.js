b506806b76138f0573d45675093da5e029f95be8
Phan Kien <phankien110302@gmail.com>
2023-09-12 09:58:16 +0700

    update

--- client/src/configs/httpRequesr.js
+++ client/src/configs/httpRequesr.js
@@ -3,7 +3,7 @@ import axios from "axios"
 const baseURLDev = "http://localhost:8081/api/"
 const baseURLProduct = "https://serverquanlyvattu.onrender.com/api/"
 const instance = axios.create({
-    baseURL: baseURLDev  ,
+    baseURL: baseURLProduct  ,
     timeout: 1000000,
     headers: { "X-Custom-Header": "foobar" },
     withCredentials: true,
